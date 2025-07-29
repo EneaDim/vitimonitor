173174ed613e1737040ec3b71509de06d63cde93
EneaDim <eneadim95@gmail.com>
2025-07-29 12:42:59 +0200

    Clean dir

--- zephyr-feasibility-emul/utils/qemu_esp32s3.sh
+++
@@ -1,3 +0,0 @@
-dd if=/dev/zero of=build/zephyr/zephyr_4mb.bin bs=1M count=4
-dd if=build/zephyr/zephyr.bin of=build/zephyr/zephyr_4mb.bin conv=notrunc
-qemu-system-xtensa -nographic -machine esp32s3 -drive file=build/zephyr/zephyr_4mb.bin,if=mtd,format=raw
